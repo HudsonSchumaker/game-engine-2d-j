3350ffcb36e2956056aae0c9cd9af025e74b475d
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-24 00:30:19 +0100

    Change to use Texture.

--- src/main/java/com/schumakerteam/alpha/core/impl/AssetManager.java
+++ src/main/java/com/schumakerteam/alpha/core/impl/AssetManager.java
@@ -2,30 +2,41 @@ package com.schumakerteam.alpha.core.impl;
 
 import com.schumakerteam.alpha.common.GeImageLoader;
 import com.schumakerteam.alpha.common.Pair;
+import com.schumakerteam.alpha.gfx.Texture;
 import com.schumakerteam.alpha.log.LogService;
 
-import java.awt.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public final class AssetManager {
-    private static Map<String, Image> IMAGE_CACHE = new HashMap<>();
+    private static Map<String, Texture> TEXTURE_CACHE = new HashMap<>();
 
     public AssetManager() {
         LogService.getInstance().engine("AssetManager created.");
     }
 
-    public static void addImage(String fileName) {
-        var image = new GeImageLoader().readFromDisk(fileName);
-        IMAGE_CACHE.put(fileName, image);
+    public static void addTexture(String fileName) {
+        var image = new GeImageLoader().readBufferImageFromDisk(fileName);
+        if (image != null)
+            TEXTURE_CACHE.put(fileName, new Texture(image.getWidth(), image.getHeight(), image));
     }
 
-    public static Image getImage(String assetId) {
-        return IMAGE_CACHE.get(assetId);
+    public static Texture getTexture(String assetId) {
+        return TEXTURE_CACHE.get(assetId);
     }
 
     public static Pair<Integer, Integer> getImageDimension(String assetId) {
-        var image = new GeImageLoader().readBufferFromDisk(assetId);
-        return Pair.of(image.getWidth(null), image.getHeight(null));
+        var texture = getTexture(assetId);
+        return Pair.of(texture.getWidth(), texture.getHeight());
+    }
+
+    public static CompletableFuture<Void> loadInBatch(List<String> fileNames) {
+        return CompletableFuture.runAsync(() -> {
+            for (var fileName : fileNames) {
+                addTexture(fileName);
+            }
+        });
     }
 }
