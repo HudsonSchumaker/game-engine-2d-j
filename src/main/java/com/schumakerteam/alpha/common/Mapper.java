7963be22c3199daf98074353f7294842f211634d
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-25 18:21:56 +0100

    Mapper

---
+++ src/main/java/com/schumakerteam/alpha/common/Mapper.java
@@ -0,0 +1,36 @@
+package com.schumakerteam.alpha.common;
+
+import com.schumakerteam.alpha.log.LogService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public interface Mapper<S, T> {
+
+    default T from(S source) {
+        return null;
+    }
+
+    default List<T> from(Iterable<S> sources) {
+        return from(sources, (s, t) -> {
+        });
+    }
+
+    default List<T> from(Iterable<S> sources, BiConsumer<S, T> postProcessor) {
+        List<T> targetList = new ArrayList<>();
+        for (S entity : sources) {
+            try {
+                T dto = from(entity);
+                postProcessor.accept(entity, dto);
+                targetList.add(dto);
+            } catch (RuntimeException e) {
+                LogService.getInstance().warning("Cannot map, skipping");
+            }
+        }
+        return targetList;
+    }
+
+    default void map(S source, T target) {
+    }
+}
---
+++ src/main/java/com/schumakerteam/alpha/gfx/Image2BufferedImageMap.java
@@ -0,0 +1,20 @@
+package com.schumakerteam.alpha.gfx;
+
+import com.schumakerteam.alpha.common.Mapper;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Image2BufferedImageMap implements Mapper<Image, BufferedImage> {
+
+    @Override
+    public BufferedImage from(Image source) {
+        BufferedImage target = new BufferedImage(source.getWidth(null), source.getHeight(null),
+                BufferedImage.TYPE_INT_ARGB);
+
+        Graphics g = target.createGraphics();
+        g.drawImage(source, 0, 0, null);
+        g.dispose();
+        return target;
+    }
+}
