23f929429bda09f2affec428cc5de0efc7ddc36e
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-25 18:22:17 +0100

    Image loader

--- src/main/java/com/schumakerteam/alpha/io/GeImageLoader.java
+++ src/main/java/com/schumakerteam/alpha/io/GeImageLoader.java
@@ -1,12 +1,16 @@
-package com.schumakerteam.alpha.common;
+package com.schumakerteam.alpha.io;
+
+import com.schumakerteam.alpha.gfx.Image2BufferedImageMap;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 
 public final class GeImageLoader {
+
     public Image readFromDisk(String fileName) {
         return Toolkit.getDefaultToolkit()
                 .getImage(this.getClass()
@@ -20,5 +24,18 @@ public final class GeImageLoader {
         } catch (IOException ignore) {
             return null;
         }
+    }
+
+    public BufferedImage readFromWeb(String url) {
+        URL www = null;
+        try {
+            www = new URL(url);
+            Image image = ImageIO.read(www);
+
+            var mapper = new Image2BufferedImageMap();
+            return mapper.from(image);
+        } catch (IOException ignore) {
+        }
+        return null;
     }
 }
