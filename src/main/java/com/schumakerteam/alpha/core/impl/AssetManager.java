1b6a0656c62e210d1469da3ef939e5495de5e4b0
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-25 21:19:24 +0100

    Changes.

--- src/main/java/com/schumakerteam/alpha/core/impl/AssetManager.java
+++ src/main/java/com/schumakerteam/alpha/core/impl/AssetManager.java
@@ -1,10 +1,13 @@
 package com.schumakerteam.alpha.core.impl;
 
+import com.schumakerteam.alpha.common.FileUtils;
+import com.schumakerteam.alpha.gfx.TileMap;
 import com.schumakerteam.alpha.io.GeImageLoader;
 import com.schumakerteam.alpha.common.Pair;
 import com.schumakerteam.alpha.gfx.Texture;
 import com.schumakerteam.alpha.log.LogService;
 
+import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,9 +15,16 @@ import java.util.concurrent.CompletableFuture;
 
 public final class AssetManager {
     private static Map<String, Texture> TEXTURE_CACHE = new HashMap<>();
+    private static Map<String, TileMap> TILEMAP_CACHE = new HashMap<>();
 
     public AssetManager() {
         LogService.getInstance().engine("AssetManager created.");
+    }
+
+    public static void addTextureFromWeb(String url) {
+        var image = new GeImageLoader().readFromWeb(url);
+        var fileName = FileUtils.getName(url);
+        TEXTURE_CACHE.put(fileName, new Texture(image.getWidth(), image.getHeight(), image));
     }
 
     public static void addTexture(String fileName) {
@@ -23,11 +33,11 @@ public final class AssetManager {
             TEXTURE_CACHE.put(fileName, new Texture(image.getWidth(), image.getHeight(), image));
     }
 
-    public static Texture getTexture(String assetId) {
+    public static Texture getTexture(final String assetId) {
         return TEXTURE_CACHE.get(assetId);
     }
 
-    public static Pair<Integer, Integer> getImageDimension(String assetId) {
+    public static Pair<Integer, Integer> getTextureDimension(String assetId) {
         var texture = getTexture(assetId);
         return Pair.of(texture.getWidth(), texture.getHeight());
     }
@@ -38,5 +48,11 @@ public final class AssetManager {
                 addTexture(fileName);
             }
         });
+    }
+
+    public static void addTileMap(int tileWidth, int tileHeight, String fileName) {
+        var image = new GeImageLoader().readBufferImageFromDisk(fileName);
+        if (image != null)
+            TILEMAP_CACHE.put(fileName, new TileMap(tileWidth, tileHeight, image.getWidth(), image.getTileHeight(), image));
     }
 }
