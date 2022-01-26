50c565621c29d3be3b3f99a73f6448dadc1223e9
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-26 21:29:20 +0100

    Tile map reader

--- src/main/java/com/schumakerteam/alpha/io/TileMapReader.java
+++ src/main/java/com/schumakerteam/alpha/io/TileMapReader.java
@@ -1,15 +1,33 @@
 package com.schumakerteam.alpha.io;
 
-import java.io.File;
-import java.util.Objects;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import static com.schumakerteam.alpha.common.Commons.TILEMAP_PATH;
 
 public final class TileMapReader {
 
-    public static final
+    private static final String COMMA_DELIMITER = ";";
 
-    public void loadTileMap(String fileName) {
-        var file = new File(Objects.requireNonNull(this.getClass()
-                .getResourceAsStream(path + fileName)));
+    public void loadTileMap(String fileName) throws IOException {
+        var stream = this.getClass().getResourceAsStream(TILEMAP_PATH + fileName);
+        assert stream != null;
+
+        var isr = new InputStreamReader(stream, StandardCharsets.UTF_8);
+        var buffer = new BufferedReader(isr);
+        var line = buffer.readLine();
+
+        while(buffer.readLine() != null) {
+            line = buffer.readLine();
+            line.split(COMMA_DELIMITER);
+        }
+
+    }
+
+    private void setValue(String[] split) {
+
+
     }
 
 }
