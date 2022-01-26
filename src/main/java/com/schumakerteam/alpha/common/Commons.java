1967399ba20936e61ec787ab333b1e031979eb76
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-26 21:28:34 +0100

    Commons

--- src/main/java/com/schumakerteam/alpha/common/Commons.java
+++ src/main/java/com/schumakerteam/alpha/common/Commons.java
@@ -2,5 +2,8 @@ package com.schumakerteam.alpha.common;
 
 public final class Commons {
 
+    public static final String IMAGES_PATH = "/resources/images/";
+    public static final String TILEMAP_PATH = "/resources/tilemap/";
+
     private Commons() {}
 }
