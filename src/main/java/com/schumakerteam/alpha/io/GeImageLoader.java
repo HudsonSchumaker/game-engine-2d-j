d316e0b42f3e22699db186844a7ece8e496e03d0
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-26 21:29:07 +0100

    Image loader.

--- src/main/java/com/schumakerteam/alpha/io/GeImageLoader.java
+++ src/main/java/com/schumakerteam/alpha/io/GeImageLoader.java
@@ -1,6 +1,6 @@
 package com.schumakerteam.alpha.io;
 
-import com.schumakerteam.alpha.gfx.Image2BufferedImageMap;
+import com.schumakerteam.alpha.gfx.Image2BufferedImageMapper;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -9,30 +9,41 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
 
+import static com.schumakerteam.alpha.common.Commons.IMAGES_PATH;
+import static com.schumakerteam.alpha.common.Commons.TILEMAP_PATH;
+
 public final class GeImageLoader {
 
     public Image readFromDisk(String fileName) {
         return Toolkit.getDefaultToolkit()
                 .getImage(this.getClass()
-                        .getResource("/resources/images/" + fileName));
+                        .getResource(IMAGES_PATH + fileName));
     }
 
-    public BufferedImage readBufferImageFromDisk(String fileName) {
+    public BufferedImage readImageFromDisk(String fileName) {
+        return readBufferImageFromDisk(IMAGES_PATH, fileName);
+    }
+
+    public BufferedImage readTileMapTexture(String fileName) {
+        return readBufferImageFromDisk(TILEMAP_PATH, fileName);
+    }
+
+    private BufferedImage readBufferImageFromDisk(String path, String fileName) {
         try {
             return ImageIO.read(Objects.requireNonNull(this.getClass()
-                    .getResourceAsStream("/resources/images/" + fileName)));
+                    .getResourceAsStream(path + fileName)));
         } catch (IOException ignore) {
             return null;
         }
     }
 
     public BufferedImage readFromWeb(String url) {
-        URL www = null;
         try {
-            www = new URL(url);
-            Image image = ImageIO.read(www);
-            return new Image2BufferedImageMap().from(image);
-        } catch (IOException ignore) {}
-        return null;
+            var www = new URL(url);
+            var image = ImageIO.read(www);
+            return new Image2BufferedImageMapper().from(image);
+        } catch (IOException ignore) {
+            return null;
+        }
     }
 }
