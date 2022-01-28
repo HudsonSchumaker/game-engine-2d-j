e9b7f98babe465c874bedda75f61b21aad2a3966
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-28 21:16:08 +0100

    TileComponent

--- src/main/java/com/schumakerteam/alpha/component/TileComponent.java
+++ src/main/java/com/schumakerteam/alpha/component/TileComponent.java
@@ -7,7 +7,10 @@ public class TileComponent extends Component {
     public static final int COMPONENT_TYPE_ID = 2;
     private final int id;
 
-    //TODO
+    int tileSize = 32;
+    double tileScale = 2.0;
+    int mapNumCols = 25;
+    int mapNumRows = 20;
 
     public TileComponent() {
         this.id = Registry.getInstance().getComponentId();
