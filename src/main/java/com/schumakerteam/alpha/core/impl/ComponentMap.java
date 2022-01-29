b0e247cba60c6a0a1502c138ce1958cb26d0f060
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-30 00:02:48 +0100

    ComponentMap changes.

--- src/main/java/com/schumakerteam/alpha/core/impl/ComponentMap.java
+++ src/main/java/com/schumakerteam/alpha/core/impl/ComponentMap.java
@@ -1,9 +1,6 @@
 package com.schumakerteam.alpha.core.impl;
 
-import com.schumakerteam.alpha.component.Component;
-import com.schumakerteam.alpha.component.RigidBodyComponent;
-import com.schumakerteam.alpha.component.SpriteComponent;
-import com.schumakerteam.alpha.component.TransformComponent;
+import com.schumakerteam.alpha.component.*;
 import com.schumakerteam.alpha.core.IPool;
 import com.schumakerteam.alpha.log.LogService;
 
@@ -14,6 +11,7 @@ public final class ComponentMap {
     private static Pool<TransformComponent> transformComponentPool = new Pool<>();
     private static Pool<RigidBodyComponent> rigidBodyComponentPool = new Pool<>();
     private static Pool<SpriteComponent> spriteComponentPool = new Pool<>();
+    private static Pool<TileMapComponent> tileMapComponentPool = new Pool<>();
 
     private static final HashMap<Integer, IPool<Component>> COMPONENT_POOL = new HashMap<>();
 
@@ -21,6 +19,7 @@ public final class ComponentMap {
         COMPONENT_POOL.put(TransformComponent.COMPONENT_TYPE_ID, transformComponentPool);
         COMPONENT_POOL.put(RigidBodyComponent.COMPONENT_TYPE_ID, rigidBodyComponentPool);
         COMPONENT_POOL.put(SpriteComponent.COMPONENT_TYPE_ID, spriteComponentPool);
+        COMPONENT_POOL.put(TileMapComponent.COMPONENT_TYPE_ID, tileMapComponentPool);
     }
 
     public ComponentMap() {
