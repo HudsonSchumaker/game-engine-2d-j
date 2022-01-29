44fbba5cd8d7ffd63d077b35445cbe0104165f89
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-29 17:39:09 +0100

    TileMapSystem

--- src/main/java/com/schumakerteam/alpha/systems/TileMapSystem.java
+++ src/main/java/com/schumakerteam/alpha/systems/TileMapSystem.java
@@ -2,7 +2,9 @@ package com.schumakerteam.alpha.systems;
 
 import com.schumakerteam.alpha.component.SpriteComponent;
 import com.schumakerteam.alpha.component.TileComponent;
+import com.schumakerteam.alpha.component.TileMapComponent;
 import com.schumakerteam.alpha.component.TransformComponent;
+import com.schumakerteam.alpha.core.impl.AssetManager;
 import com.schumakerteam.alpha.ecs.impl.BasicSystem;
 import com.schumakerteam.alpha.ecs.impl.Registry;
 import com.schumakerteam.alpha.log.LogService;
@@ -11,13 +13,13 @@ import java.awt.*;
 
 public class TileMapSystem extends BasicSystem {
 
-    public static final int SYSTEM_TYPE_ID = 1;
+    public static final int SYSTEM_TYPE_ID = 2;
     private final int id;
     private Graphics2D render;
 
     public TileMapSystem() {
         this.id = Registry.getInstance().getSystemId();
-        this.setOnSignature(TileComponent.COMPONENT_TYPE_ID);
+        this.setOnSignature(TileMapComponent.COMPONENT_TYPE_ID);
         this.setOnSignature(TransformComponent.COMPONENT_TYPE_ID);
         LogService.getInstance().engine("TileMapSystem created with id: " + id);
     }
@@ -29,7 +31,23 @@ public class TileMapSystem extends BasicSystem {
 
     @Override
     protected void update() {
-        //TODO
+        for (var entity : getSystemEntities()) {
+            var transform = (TransformComponent) entity.getComponent(TransformComponent.COMPONENT_TYPE_ID);
+            var tileMap = (TileMapComponent) entity.getComponent(TileMapComponent.COMPONENT_TYPE_ID);
+
+            var image = AssetManager.getTileMap(tileMap.getSpriteName()).getBufferedImage();
+
+            for (var tile : tileMap.getTiles()) {
+                var tileImage = image.getSubimage(tile.getTileX(), tile.getTileY(), tileMap.getTileSize(), tileMap.getTileSize());
+                render.drawImage(
+                        tileImage,
+                        (int) (transform.getPosition().getX() + tile.getTransformComponent().getPosition().getX()),
+                        (int) (transform.getPosition().getY() + tile.getTransformComponent().getPosition().getY()),
+                        tileImage.getWidth() ,
+                        tileImage.getHeight(),
+                        null);
+            }
+        }
     }
 
     @Override
