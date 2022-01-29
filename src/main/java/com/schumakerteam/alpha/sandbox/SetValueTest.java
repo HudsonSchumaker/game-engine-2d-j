a3007363f9c384b39d03253334fb909377cdfbe8
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-29 17:38:47 +0100

    test

---
+++ src/main/java/com/schumakerteam/alpha/sandbox/SetValueTest.java
@@ -0,0 +1,16 @@
+package com.schumakerteam.alpha.sandbox;
+
+public class SetValueTest {
+
+    public static void main(String ...args) {
+        var line = "12,22,33,44,55";
+        var split = line.split(",");
+
+        for (var pos : split) {
+            var posX = pos.substring(0, 1);
+            var posY = pos.substring(1, 2);
+
+            var x = 0;
+        }
+    }
+}
