39517dd4206cba5958d9ac8df52154289a013f69
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-03 21:09:39 +0100

    GeImageLoader new method.

--- src/main/java/com/schumakerteam/alpha/io/GeImageLoader.java
+++ src/main/java/com/schumakerteam/alpha/io/GeImageLoader.java
@@ -1,6 +1,7 @@
 package com.schumakerteam.alpha.io;
 
 import com.schumakerteam.alpha.gfx.Image2BufferedImageMapper;
+import com.schumakerteam.alpha.log.LogService;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -22,18 +23,29 @@ public final class GeImageLoader {
     }
 
     public Image readImageFromDisk(String fileName) {
-        return readBufferImageFromDisk(IMAGES_PATH, fileName);
+        return readImageFromDisk(IMAGES_PATH, fileName);
     }
 
     public Image readTileMapTexture(String fileName) {
-        return readBufferImageFromDisk(TILEMAP_PATH, fileName);
+        return readImageFromDisk(TILEMAP_PATH, fileName);
     }
 
-    private Image readBufferImageFromDisk(String path, String fileName) {
+    private Image readImageFromDisk(String path, String fileName) {
         try {
             return this.createAcceleratedImage(ImageIO.read(Objects.requireNonNull(this.getClass()
                     .getResourceAsStream(path + fileName))));
         } catch (IOException ignore) {
+            LogService.getInstance().error("Could not read image: " + fileName);
+            return null;
+        }
+    }
+
+    public BufferedImage readBufferedImage(String path, String fileName) {
+        try {
+            return ImageIO.read(Objects.requireNonNull(this.getClass()
+                    .getResourceAsStream(path + fileName)));
+        } catch (IOException ignore) {
+            LogService.getInstance().error("Could not read image: " + fileName);
             return null;
         }
     }
@@ -44,11 +56,12 @@ public final class GeImageLoader {
             var rawImage = ImageIO.read(www);
             return this.createAcceleratedImage(new Image2BufferedImageMapper().from(rawImage));
         } catch (IOException ignore) {
+            LogService.getInstance().error("Could not read image: " + url);
             return null;
         }
     }
 
-    private Image createAcceleratedImage(BufferedImage source) {
+    public Image createAcceleratedImage(BufferedImage source) {
         GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
         Image image = gc.createCompatibleImage(source.getWidth(), source.getHeight(), Transparency.TRANSLUCENT);
 
