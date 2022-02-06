c012bc0918f7b86da72dbfa5aef8d2969b90da87
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-06 17:42:31 +0100

    added tileTexture creation.

--- src/main/java/com/schumakerteam/alpha/io/TileMapReader.java
+++ src/main/java/com/schumakerteam/alpha/io/TileMapReader.java
@@ -2,9 +2,12 @@ package com.schumakerteam.alpha.io;
 
 import com.schumakerteam.alpha.component.TileComponent;
 import com.schumakerteam.alpha.component.TransformComponent;
+import com.schumakerteam.alpha.core.impl.AssetManager;
 import com.schumakerteam.alpha.geometry.Scale2D;
 import com.schumakerteam.alpha.geometry.Vector2D;
+import com.schumakerteam.alpha.gfx.TileTexture;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -17,20 +20,28 @@ import static com.schumakerteam.alpha.common.Commons.TILEMAP_PATH;
 public final class TileMapReader {
 
     private static final String COMMA_DELIMITER = ",";
+    private final GeImageLoader imageLoader;
 
-    public List<TileComponent> loadTileMap(String fileName, Scale2D scale, Integer tileSize) throws IOException {
+    public TileMapReader() {
+        this.imageLoader = new GeImageLoader();
+    }
+
+    public List<TileComponent> loadTileMap(String fileName, String spriteName, Scale2D scale, Integer tileSize) throws IOException {
         var stream = this.getClass().getResourceAsStream(TILEMAP_PATH + fileName);
         assert stream != null;
 
         var isr = new InputStreamReader(stream, StandardCharsets.UTF_8);
         var buffer = new BufferedReader(isr);
 
+        var row = 0;
         List<TileComponent> tiles = new ArrayList<>();
-        int row = 0;
-        while(buffer.readLine() != null) {
+        while (buffer.readLine() != null) {
             this.createTile(buffer.readLine(), tiles, scale, tileSize, row);
             row++;
         }
+
+        var mapTexture = imageLoader.readBufferedImage(TILEMAP_PATH, spriteName);
+        this.createTileTexture(tiles, tileSize, spriteName, mapTexture);
 
         buffer.close();
         isr.close();
@@ -40,7 +51,7 @@ public final class TileMapReader {
     }
 
     private void createTile(String line, List<TileComponent> tiles, Scale2D scale, Integer tileSize, int row) {
-        var split= line.split(COMMA_DELIMITER);
+        var split = line.split(COMMA_DELIMITER);
         for (int column = 0; column < split.length; column++) {
             var posX = split[column].substring(0, 1);
             var posY = split[column].substring(1, 2);
@@ -50,12 +61,32 @@ public final class TileMapReader {
             int tileX = Integer.parseInt(posY) * tileSize;
 
             var transform = new TransformComponent(
-                    new Vector2D(column * (scale.getX() * tileSize),row * (scale.getY() * tileSize)),
+                    new Vector2D(column * (scale.getX() * tileSize), row * (scale.getY() * tileSize)),
                     scale,
                     0.0
             );
             var tileComponent = new TileComponent(tileX, tileY, transform);
             tiles.add(tileComponent);
         }
+    }
+
+    private void createTileTexture(List<TileComponent> tiles, Integer tileSize, String spriteName, BufferedImage mapTexture) {
+        for (int k = 0; k < tiles.size(); k++) {
+            var tile = tiles.get(k);
+            var tileImage = mapTexture.getSubimage(
+                    tile.getTileX(),
+                    tile.getTileY(),
+                    tileSize,
+                    tileSize);
+
+            var acceleratedImage = imageLoader.createAcceleratedImage(tileImage);
+            var name = this.createTileTextureName(spriteName, tile.getTransformComponent(), k);
+            tile.setTileName(name);
+            AssetManager.addTileTexture(name, new TileTexture(acceleratedImage));
+        }
+    }
+
+    private String createTileTextureName(String spriteName, TransformComponent transform, Integer index) {
+        return spriteName + "-" + transform.getX() + "_" + transform.getY() + "-" + index;
     }
 }
