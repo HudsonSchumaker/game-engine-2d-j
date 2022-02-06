75b3840ec54bf123f4798e1bbff608ed893b8372
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-06 17:10:15 +0100

    Organize imports.

--- src/main/java/com/schumakerteam/alpha/gfx/BufferedImage2ImageMapper.java
+++ src/main/java/com/schumakerteam/alpha/gfx/BufferedImage2ImageMapper.java
@@ -3,7 +3,7 @@ package com.schumakerteam.alpha.gfx;
 import com.schumakerteam.alpha.common.Mapper;
 import com.schumakerteam.alpha.io.GeImageLoader;
 
-import java.awt.*;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 public class BufferedImage2ImageMapper implements Mapper<BufferedImage, Image> {
