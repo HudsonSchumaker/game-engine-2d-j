386f22c61b13693c224b45d46e42e3d4d16267d7
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-08 21:13:37 +0100

    AnimationComponent add methods.

--- src/main/java/com/schumakerteam/alpha/component/AnimationComponent.java
+++ src/main/java/com/schumakerteam/alpha/component/AnimationComponent.java
@@ -1,4 +1,69 @@
 package com.schumakerteam.alpha.component;
 
-public class AnimationComponent {
+import com.schumakerteam.alpha.ecs.impl.Registry;
+import com.schumakerteam.alpha.log.LogService;
+
+public class AnimationComponent extends Component {
+
+    public static final int COMPONENT_TYPE_ID = 4;
+    private final int id;
+
+    private final int numberFrames;
+    private int currentFrame;
+    private int animationSpeed;
+    private final boolean loop;
+
+    public AnimationComponent(int numberFrames, int currentFrame, int animationSpeed, boolean loop) {
+        this.id = Registry.getInstance().getComponentId();
+        this.numberFrames = numberFrames;
+        this.currentFrame = currentFrame;
+        this.animationSpeed = animationSpeed;
+        this.loop = loop;
+        LogService.getInstance().engine("AnimationComponent created with id: " + id);
+    }
+
+    public int getNumberFrames() {
+        return numberFrames;
+    }
+
+    public int getCurrentFrame() {
+        return currentFrame;
+    }
+
+    public int getAnimationSpeed() {
+        return animationSpeed;
+    }
+
+    public boolean isLoop() {
+        return loop;
+    }
+
+    public void setCurrentFrame(int currentFrame) {
+        this.currentFrame = currentFrame;
+    }
+
+    public void setAnimationSpeed(int animationSpeed) {
+        this.animationSpeed = animationSpeed;
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public int getTypeId() {
+        return COMPONENT_TYPE_ID;
+    }
+
+    @Override
+    public String toString() {
+        return "AnimationComponent{" +
+                "id=" + id +
+                ", numberFrames=" + numberFrames +
+                ", currentFrame=" + currentFrame +
+                ", animationSpeed=" + animationSpeed +
+                ", loop=" + loop +
+                '}';
+    }
 }
