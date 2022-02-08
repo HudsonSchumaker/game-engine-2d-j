159673f2de4013dfc455d1479c771c4e6feeb2ec
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-08 21:29:37 +0100

    refactoring.

--- src/main/java/com/schumakerteam/alpha/systems/AnimationSystem.java
+++ src/main/java/com/schumakerteam/alpha/systems/AnimationSystem.java
@@ -1,21 +1,41 @@
 package com.schumakerteam.alpha.systems;
 
+import com.schumakerteam.alpha.component.RigidBodyComponent;
+import com.schumakerteam.alpha.component.TransformComponent;
 import com.schumakerteam.alpha.ecs.impl.BasicSystem;
+import com.schumakerteam.alpha.ecs.impl.Registry;
+import com.schumakerteam.alpha.log.LogService;
+
+import java.awt.*;
 
 public class AnimationSystem extends BasicSystem {
 
+    public static final int SYSTEM_TYPE_ID = 2;
+    private final int id;
+    private Graphics2D render;
+
+    public AnimationSystem() {
+        this.id = Registry.getInstance().getSystemId();
+       // TODO set necessary components signatures.
+        LogService.getInstance().engine("AnimationSystem created with id: " + id);
+    }
+
+    public void update(Graphics2D g) {
+        this.render = g;
+        this.update();
+    }
+
     @Override
     protected void update() {
-
     }
 
     @Override
     public int getId() {
-        return 0;
+        return id;
     }
 
     @Override
     public int getTypeId() {
-        return 0;
+        return SYSTEM_TYPE_ID;
     }
 }
