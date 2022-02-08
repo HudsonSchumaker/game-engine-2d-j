8f1879b900de3687b8a6d2f33bfd3d11d664d337
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-08 21:24:32 +0100

    add super methods.

--- src/main/java/com/schumakerteam/alpha/systems/AnimationSystem.java
+++ src/main/java/com/schumakerteam/alpha/systems/AnimationSystem.java
@@ -1,4 +1,21 @@
 package com.schumakerteam.alpha.systems;
 
-public class AnimationSystem {
+import com.schumakerteam.alpha.ecs.impl.BasicSystem;
+
+public class AnimationSystem extends BasicSystem {
+
+    @Override
+    protected void update() {
+
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public int getTypeId() {
+        return 0;
+    }
 }
