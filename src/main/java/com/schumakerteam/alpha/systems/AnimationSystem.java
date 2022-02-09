0aae7fa71eed74dcdd3758d15fd19ebc1e6317d1
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-09 23:55:31 +0100

    V1

--- src/main/java/com/schumakerteam/alpha/systems/AnimationSystem.java
+++ src/main/java/com/schumakerteam/alpha/systems/AnimationSystem.java
@@ -1,34 +1,41 @@
 package com.schumakerteam.alpha.systems;
 
 import com.schumakerteam.alpha.component.AnimationComponent;
-import com.schumakerteam.alpha.component.RigidBodyComponent;
-import com.schumakerteam.alpha.component.TransformComponent;
+import com.schumakerteam.alpha.component.SpriteComponent;
+import com.schumakerteam.alpha.component.SpriteSheetComponent;
 import com.schumakerteam.alpha.ecs.impl.BasicSystem;
 import com.schumakerteam.alpha.ecs.impl.Registry;
 import com.schumakerteam.alpha.log.LogService;
 
-import java.awt.*;
 
 public class AnimationSystem extends BasicSystem {
 
-    public static final int SYSTEM_TYPE_ID = 2;
+    public static final int SYSTEM_TYPE_ID = 3;
     private final int id;
-    private Graphics2D render;
 
     public AnimationSystem() {
         this.id = Registry.getInstance().getSystemId();
-       // TODO set necessary components signatures.
+        this.setOnSignature(SpriteComponent.COMPONENT_TYPE_ID);
         this.setOnSignature(AnimationComponent.COMPONENT_TYPE_ID);
         LogService.getInstance().engine("AnimationSystem created with id: " + id);
     }
 
-    public void update(Graphics2D g) {
-        this.render = g;
+    public void update(double deltaTime) {
         this.update();
     }
 
     @Override
     protected void update() {
+        for (var entity : getSystemEntities()) {
+            var sprite = (SpriteSheetComponent) entity.getComponent(SpriteComponent.COMPONENT_TYPE_ID);
+            var animation = (AnimationComponent) entity.getComponent(AnimationComponent.COMPONENT_TYPE_ID);
+
+            long currentFrame = (System.currentTimeMillis() - animation.getStartTime())
+                    * animation.getAnimationSpeed() / 1000 % animation.getNumberFrames();
+
+            animation.setCurrentFrame((int)currentFrame);
+            sprite.setCurrentFrame(animation.getCurrentFrame());
+        }
     }
 
     @Override
