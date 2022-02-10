e8ec3e006b616c5afd781fed12cc9a30a71204fe
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-10 01:11:38 +0100

    Resolution.

--- src/main/java/com/schumakerteam/alpha/Main.java
+++ src/main/java/com/schumakerteam/alpha/Main.java
@@ -5,7 +5,7 @@ import com.schumakerteam.alpha.core.impl.Game;
 public class Main {
 
     public static void main(String ...args) {
-        Game game = new Game(800, 600);
+        Game game = new Game(1600, 900);
         game.start();
     }
 }
