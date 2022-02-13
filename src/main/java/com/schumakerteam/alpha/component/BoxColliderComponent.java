f445b770a5c6c0207b6491bf5facd375b0027836
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-13 19:57:37 +0100

    Add

--- src/main/java/com/schumakerteam/alpha/component/BoxColliderComponent.java
+++ src/main/java/com/schumakerteam/alpha/component/BoxColliderComponent.java
@@ -1,4 +1,15 @@
 package com.schumakerteam.alpha.component;
 
+import com.schumakerteam.alpha.ecs.impl.Registry;
+import com.schumakerteam.alpha.log.LogService;
+
 public class BoxColliderComponent {
+
+    public static final int COMPONENT_TYPE_ID = 4;
+    private final int id;
+
+    public BoxColliderComponent() {
+        this.id = Registry.getInstance().getComponentId();
+        LogService.getInstance().engine("BoxColliderComponent created with id: " + id);
+    }
 }
