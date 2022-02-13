c86b730dfee690709668c6750ad5a59ce2a8016e
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-13 20:09:54 +0100

    Add

--- src/main/java/com/schumakerteam/alpha/systems/CollisionSystem.java
+++ src/main/java/com/schumakerteam/alpha/systems/CollisionSystem.java
@@ -1,7 +1,9 @@
 package com.schumakerteam.alpha.systems;
 
 import com.schumakerteam.alpha.component.AnimationComponent;
+import com.schumakerteam.alpha.component.BoxColliderComponent;
 import com.schumakerteam.alpha.component.SpriteComponent;
+import com.schumakerteam.alpha.component.TransformComponent;
 import com.schumakerteam.alpha.ecs.impl.BasicSystem;
 import com.schumakerteam.alpha.ecs.impl.Registry;
 import com.schumakerteam.alpha.log.LogService;
@@ -13,13 +15,16 @@ public class CollisionSystem extends BasicSystem {
 
     public CollisionSystem() {
         this.id = Registry.getInstance().getSystemId();
-        this.setOnSignature(SpriteComponent.COMPONENT_TYPE_ID);
+        this.setOnSignature(TransformComponent.COMPONENT_TYPE_ID);
+        this.setOnSignature(BoxColliderComponent.COMPONENT_TYPE_ID);
         LogService.getInstance().engine("CollisionSystem created with id: " + id);
     }
 
     @Override
     protected void update() {
-
+        for (var entity : getSystemEntities()) {
+            //TODO AABB collision
+        }
     }
 
     @Override
