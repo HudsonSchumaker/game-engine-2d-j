c39375f374f507717ac11896c7589fa229f6f6de
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-15 21:06:51 +0100

    test box collider with offset.

--- src/main/java/com/schumakerteam/alpha/Main.java
+++ src/main/java/com/schumakerteam/alpha/Main.java
@@ -5,7 +5,7 @@ import com.schumakerteam.alpha.core.impl.Game;
 public class Main {
 
     public static void main(String ...args) {
-        Game game = new Game(1920, 1080);
+        Game game = new Game(1600, 900);
         game.start();
     }
 }
