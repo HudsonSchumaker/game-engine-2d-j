b331ae5598ece3c83e699d8a17b6020d5341779e
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-16 21:43:53 +0100

    Kill entity.

--- src/main/java/com/schumakerteam/alpha/component/SpriteComponent.java
+++ src/main/java/com/schumakerteam/alpha/component/SpriteComponent.java
@@ -1,12 +1,13 @@
 package com.schumakerteam.alpha.component;
 
+import com.schumakerteam.alpha.common.IdComponentMap;
 import com.schumakerteam.alpha.core.impl.AssetTextureManager;
 import com.schumakerteam.alpha.ecs.impl.Registry;
 import com.schumakerteam.alpha.log.LogService;
 
 public class SpriteComponent extends Component {
 
-    public static final int COMPONENT_TYPE_ID = 2;
+    public static final int COMPONENT_TYPE_ID = IdComponentMap.getTypeId(SpriteComponent.class.getName());
     private final int id;
     private final int width;
     private final int height;
