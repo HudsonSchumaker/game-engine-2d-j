7daafe9fc96c464a1ce129055f9638eacdcc8baa
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-18 00:06:18 +0100

    EventBus.

---
+++ src/main/java/com/schumakerteam/alpha/systems/DamageSystem.java
@@ -0,0 +1,38 @@
+package com.schumakerteam.alpha.systems;
+
+import com.schumakerteam.alpha.ecs.impl.BasicSystem;
+import com.schumakerteam.alpha.ecs.impl.Entity;
+import com.schumakerteam.alpha.ecs.impl.Registry;
+import com.schumakerteam.alpha.events.EventBus;
+import com.schumakerteam.alpha.events.OnCollisionEventListener;
+import com.schumakerteam.alpha.log.LogService;
+
+public class DamageSystem extends BasicSystem implements OnCollisionEventListener {
+
+    public static final int SYSTEM_TYPE_ID = 5;
+    private final int id;
+
+    public DamageSystem() {
+        this.id = Registry.getInstance().getSystemId();
+        EventBus.getInstance().addCollisionEventListener(this);
+    }
+
+    @Override
+    protected void update() {
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public int getTypeId() {
+        return SYSTEM_TYPE_ID;
+    }
+
+    @Override
+    public void onCollisionEvent(Entity a, Entity b) {
+        LogService.getInstance().info("received event");
+    }
+}
