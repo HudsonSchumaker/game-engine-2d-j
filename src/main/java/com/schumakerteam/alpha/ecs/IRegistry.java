e18a4dcb6d617d17a2b030e7c958497fe2a883d9
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-17 09:09:46 +0100

    Kill entity.

--- src/main/java/com/schumakerteam/alpha/ecs/IRegistry.java
+++ src/main/java/com/schumakerteam/alpha/ecs/IRegistry.java
@@ -5,16 +5,17 @@ import com.schumakerteam.alpha.ecs.impl.BasicSystem;
 import com.schumakerteam.alpha.ecs.impl.Entity;
 
 public interface IRegistry {
-
     void update();
     Entity createEntity();
-    void addEntityToSystems(Entity entity);
-    void removeEntityFromSystem(Entity entity);
+    void addEntityToSystems(Entity e);
+    void removeEntityFromSystem(Entity e);
     void addComponent(Entity e, Component c);
+    void removeComponents(Entity e);
     Component getComponent(Entity entity, int componentTypeId);
     void removeComponent(Entity e, int componentId);
     boolean hasComponent(Entity e, int componentId);
     boolean hasComponentType(Entity e, int componentTypeId);
+    void destroyEntity(Entity e);
     void addSystem(BasicSystem system);
     BasicSystem getSystem(int systemTypeId);
     void removeSystem(BasicSystem system);
