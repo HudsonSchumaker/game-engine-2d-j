695efe9584b9f04e80cc62ac00890b8ae4d7c215
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-17 09:10:40 +0100

    Audio System.

--- src/main/java/com/schumakerteam/alpha/common/Commons.java
+++ src/main/java/com/schumakerteam/alpha/common/Commons.java
@@ -2,6 +2,7 @@ package com.schumakerteam.alpha.common;
 
 public final class Commons {
 
+    public static final String AUDIO_PATH = "/resources/audio/";
     public static final String IMAGES_PATH = "/resources/images/";
     public static final String TILEMAP_PATH = "/resources/tilemap/";
 
