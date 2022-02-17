3a4d968af42b62c8caf04f735df1d5ab1bef7945
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-18 00:04:24 +0100

    Refactoring.

--- src/main/java/com/schumakerteam/alpha/common/Commons.java
+++ src/main/java/com/schumakerteam/alpha/common/Commons.java
@@ -6,6 +6,5 @@ public final class Commons {
     public static final String IMAGES_PATH = "/resources/images/";
     public static final String TILEMAP_PATH = "/resources/tilemap/";
 
-    private Commons() {
-    }
+    private Commons() {}
 }
