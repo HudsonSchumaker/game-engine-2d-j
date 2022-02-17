1b2715373e260ad5a0347f75d591cfa3f50f5240
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-17 09:09:50 +0100

    Kill entity.

--- src/main/java/com/schumakerteam/alpha/Main.java
+++ src/main/java/com/schumakerteam/alpha/Main.java
@@ -5,7 +5,7 @@ import com.schumakerteam.alpha.core.impl.Game;
 public class Main {
 
     public static void main(String ...args) {
-        Game game = new Game(1280, 960);
+        Game game = new Game(1600, 480);
         game.start();
     }
 }
