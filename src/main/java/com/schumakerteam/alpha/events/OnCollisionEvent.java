7a65d36cec4305c1e507c8df584b6fb2db76cea5
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-19 19:12:21 +0100

    Event.

---
+++ src/main/java/com/schumakerteam/alpha/events/OnCollisionEvent.java
@@ -0,0 +1,22 @@
+package com.schumakerteam.alpha.events;
+
+import com.schumakerteam.alpha.ecs.impl.Entity;
+
+public class OnCollisionEvent implements Event {
+
+    private final Entity a;
+    private final Entity b;
+
+    public OnCollisionEvent(Entity a, Entity b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Entity getA() {
+        return a;
+    }
+
+    public Entity getB() {
+        return b;
+    }
+}
