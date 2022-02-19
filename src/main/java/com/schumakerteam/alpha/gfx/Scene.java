a25837f8070b88b053f6beca0ad53557cccab4bb
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-20 00:05:10 +0100

    Refactoring.

--- src/main/java/com/schumakerteam/alpha/gfx/Scene.java
+++ src/main/java/com/schumakerteam/alpha/gfx/Scene.java
@@ -1,18 +1,27 @@
 package com.schumakerteam.alpha.gfx;
 
+import com.schumakerteam.alpha.common.KeyboardInput;
+
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 
 public final class Scene extends Canvas {
 
     private static final int numberOfBuffers = 2;
+    private KeyboardInput keys;
 
     public Scene(int width, int height) {
         this.setIgnoreRepaint(true);
         this.setSize(width, height);
+        this.keys = new KeyboardInput();
+        this.addKeyListener(keys);
     }
 
     public void initialize() {
         this.createBufferStrategy(numberOfBuffers);
+    }
+
+    public KeyboardInput getKeys() {
+        return this.keys;
     }
 }
