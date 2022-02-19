dbbec8af0a764ec0f1f43eccc5abaac1f427f37a
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-19 21:03:36 +0100

    EventBus.

--- src/main/java/com/schumakerteam/alpha/events/OnCollisionEventListener.java
+++
@@ -1,7 +0,0 @@
-package com.schumakerteam.alpha.events;
-
-import com.schumakerteam.alpha.ecs.impl.Entity;
-
-public interface OnCollisionEventListener {
-    void onCollisionEvent(Entity a, Entity b);
-}
