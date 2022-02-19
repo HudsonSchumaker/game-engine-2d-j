150c1ef2e5cac23f7fa4964a3460815a193b92d4
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-19 19:12:18 +0100

    Event.

--- src/main/java/com/schumakerteam/alpha/events/OnCollisionEventPublisher.java
+++
@@ -1,7 +0,0 @@
-package com.schumakerteam.alpha.events;
-
-import com.schumakerteam.alpha.ecs.impl.Entity;
-
-public interface OnCollisionEventPublisher {
-    void onCollisionEvent(Entity a, Entity b);
-}
