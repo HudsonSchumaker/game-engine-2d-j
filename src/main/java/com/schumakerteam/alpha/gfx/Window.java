b32ad5a263d4fa299639f46551ba563cd308fc2d
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-20 00:05:12 +0100

    Refactoring.

--- src/main/java/com/schumakerteam/alpha/gfx/Window.java
+++ src/main/java/com/schumakerteam/alpha/gfx/Window.java
@@ -1,24 +1,36 @@
 package com.schumakerteam.alpha.gfx;
 
+import com.schumakerteam.alpha.events.EventBus;
+import com.schumakerteam.alpha.events.EventType;
+import com.schumakerteam.alpha.events.OnWindowClosingEvent;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public final class Window extends JFrame {
 
-    public Window(Canvas canvas) {
-        this.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+    public Window(Scene scene) {
+        this.setBounds(0, 0, scene.getWidth(), scene.getHeight());
         JPanel panel = (JPanel) this.getContentPane();
         panel.setLayout(null);
-        panel.setPreferredSize(new Dimension(canvas.getWidth(), canvas.getHeight()));
+        panel.setPreferredSize(new Dimension(scene.getWidth(), scene.getHeight()));
         panel.setIgnoreRepaint(true);
-        panel.add(canvas);
+        panel.add(scene);
 
         this.setIgnoreRepaint(true);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setUndecorated(true);
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                EventBus.getInstance().publish(EventType.ON_WINDOW_CLOSING, new OnWindowClosingEvent());
+            }
+        });
+        //this.setUndecorated(true);
         this.setLayout(null);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+        scene.initialize();
     }
 }
