6f925813f0e72459adeb9bff011b02b62775f592
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-19 19:11:45 +0100

    Log

--- src/main/java/com/schumakerteam/alpha/log/LogService.java
+++ src/main/java/com/schumakerteam/alpha/log/LogService.java
@@ -1,11 +1,6 @@
 package com.schumakerteam.alpha.log;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 public final class LogService implements ILogService {
 
@@ -17,7 +12,13 @@ public final class LogService implements ILogService {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_BLUE = "\u001B[34m";
 
-    private List<LogEntry> messages = new ArrayList();
+    private static final int CORE_POOL_SIZE = 1;
+    private static final int MAXIMUM_POOL_SIZE = 1;
+    private static final long KEEP_ALIVE_TIME = 5;
+
+    private final BlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
+    private final ExecutorService executor =
+            new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, blockingQueue);
 
     private LogService() {
     }
@@ -29,30 +30,24 @@ public final class LogService implements ILogService {
     @Override
     public void info(String message) {
         var log = new LogEntry(LogTypeEnum.LOG_INFO, ANSI_GREEN + "LOG [" + DateUtil.getLogDate() + "] " + message + ANSI_RESET);
-        System.out.println(log.getMessage());
+        executor.submit(log);
     }
 
     @Override
     public void warning(String message) {
-        new Thread(() -> {
-            var log = new LogEntry(LogTypeEnum.LOG_WARNING, ANSI_YELLOW + "LOG [" + DateUtil.getLogDate() + "] " + message + ANSI_RESET);
-            System.out.println(log.getMessage());
-            messages.add(log);
-        }).start();
+        var log = new LogEntry(LogTypeEnum.LOG_WARNING, ANSI_YELLOW + "LOG [" + DateUtil.getLogDate() + "] " + message + ANSI_RESET);
+        executor.submit(log);
     }
 
     @Override
     public void error(String message) {
-        new Thread(() -> {
-            var log = new LogEntry(LogTypeEnum.LOG_ERROR, ANSI_RED + "LOG [" + DateUtil.getLogDate() + "] " + message + ANSI_RESET);
-            System.out.println(log.getMessage());
-            messages.add(log);
-        }).start();
+        var log = new LogEntry(LogTypeEnum.LOG_ERROR, ANSI_RED + "LOG [" + DateUtil.getLogDate() + "] " + message + ANSI_RESET);
+        executor.submit(log);
     }
 
     @Override
     public void engine(String message) {
         var log = new LogEntry(LogTypeEnum.LOG_ENGINE, ANSI_BLUE + "LOG [" + DateUtil.getLogDate() + "] " + message + ANSI_RESET);
-        System.out.println(log.getMessage());
+        executor.submit(log);
     }
 }
