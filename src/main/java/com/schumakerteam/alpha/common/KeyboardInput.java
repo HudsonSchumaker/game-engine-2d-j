706c31658726f0004954374256532b9406893b38
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-20 00:04:50 +0100

    Added.

---
+++ src/main/java/com/schumakerteam/alpha/common/KeyboardInput.java
@@ -0,0 +1,52 @@
+package com.schumakerteam.alpha.common;
+
+import java.awt.event.*;
+
+public class KeyboardInput implements KeyListener {
+
+    private boolean[] keys;
+    private int[] polled;
+
+    public KeyboardInput() {
+        keys = new boolean[256];
+        polled = new int[256];
+    }
+
+    public boolean keyDown(int keyCode) {
+        return polled[keyCode] > 0;
+    }
+
+    public boolean keyDownOnce(int keyCode) {
+        return polled[keyCode] == 1;
+    }
+
+    public synchronized void poll() {
+        for (int i = 0; i < keys.length; ++i) {
+            if (keys[i]) {
+                polled[i]++;
+            } else {
+                polled[i] = 0;
+            }
+        }
+    }
+
+    @Override
+    public synchronized void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        if (keyCode >= 0 && keyCode < keys.length) {
+            keys[keyCode] = true;
+        }
+    }
+
+    @Override
+    public synchronized void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        if (keyCode >= 0 && keyCode < keys.length) {
+            keys[keyCode] = false;
+        }
+    }
+
+    @Override
+    public synchronized void keyTyped(KeyEvent ignore) {
+    }
+}
