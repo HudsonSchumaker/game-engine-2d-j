0b0a2ab994516b02f7716cef2198329ddd29ca60
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-20 00:03:21 +0100

    EventBus.

--- src/main/java/com/schumakerteam/alpha/systems/CollisionSystem.java
+++ src/main/java/com/schumakerteam/alpha/systems/CollisionSystem.java
@@ -38,7 +38,7 @@ public class CollisionSystem extends BasicSystem {
                 }
 
                 if (this.collide(aEntity, bEntity)) {
-                    EventBus.getInstance().notify(EventType.ON_COLLISION_EVENT, new OnCollisionEvent(aEntity, bEntity));
+                    EventBus.getInstance().publish(EventType.ON_COLLISION_EVENT, new OnCollisionEvent(aEntity, bEntity));
                 }
             }
         }
