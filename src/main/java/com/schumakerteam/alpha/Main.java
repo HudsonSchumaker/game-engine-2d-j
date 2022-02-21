e8078e9b3f3036b45ceea75e58e776ad2033677b
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-21 12:13:08 +0100

    Refactoring.

--- src/main/java/com/schumakerteam/alpha/Main.java
+++ src/main/java/com/schumakerteam/alpha/Main.java
@@ -5,7 +5,7 @@ import com.schumakerteam.alpha.core.impl.Game;
 public class Main {
 
     public static void main(String ...args) {
-        Game game = new Game(1600, 480);
+        Game game = new Game(1600, 700);
         game.start();
     }
 }
