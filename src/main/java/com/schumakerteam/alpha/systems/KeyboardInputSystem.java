4a4f46d55bb2d99ef1078feacf83a14b0c8968d0
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-21 12:11:47 +0100

    Refactoring.

--- src/main/java/com/schumakerteam/alpha/systems/KeyboardInputSystem.java
+++ src/main/java/com/schumakerteam/alpha/systems/KeyboardInputSystem.java
@@ -1,4 +1,78 @@
 package com.schumakerteam.alpha.systems;
 
-public class KeyboardInputSystem {
+import com.schumakerteam.alpha.component.InputComponent;
+import com.schumakerteam.alpha.component.RigidBodyComponent;
+import com.schumakerteam.alpha.component.TransformComponent;
+import com.schumakerteam.alpha.ecs.impl.BasicSystem;
+import com.schumakerteam.alpha.ecs.impl.Registry;
+import com.schumakerteam.alpha.events.*;
+import com.schumakerteam.alpha.log.LogService;
+
+import java.awt.event.KeyEvent;
+
+public class KeyboardInputSystem extends BasicSystem implements EventListener {
+
+    public static final int SYSTEM_TYPE_ID = 6;
+    private final int id;
+    private double deltaTime;
+
+    public KeyboardInputSystem() {
+        this.id = Registry.getInstance().getSystemId();
+        this.setOnSignatures();
+        EventBus.getInstance().subscribe(EventType.ON_KEY_PRESSED, this);
+        LogService.getInstance().engine("KeyboardInputSystem created with id: " + id);
+    }
+
+    @Override
+    protected void setOnSignatures() {
+        this.setOnSignature(TransformComponent.COMPONENT_TYPE_ID);
+        this.setOnSignature(RigidBodyComponent.COMPONENT_TYPE_ID);
+        this.setOnSignature(InputComponent.COMPONENT_TYPE_ID);
+    }
+
+    @Override
+    public void update(double deltaTime) {
+        this.deltaTime = deltaTime;
+    }
+
+    @Override
+    public void notifyEvent(EventType eventType, Event<?> event) {
+        var onKeyPressedEvent = (OnKeyPressedEvent) event;
+
+            if (onKeyPressedEvent.getKeyCode() == KeyEvent.VK_LEFT) {
+                for (var entity : getSystemEntities()) {
+                    var transform = (TransformComponent) entity.getComponent(TransformComponent.COMPONENT_TYPE_ID);
+                    var rigidBody = (RigidBodyComponent) entity.getComponent(RigidBodyComponent.COMPONENT_TYPE_ID);
+
+                    transform.getPosition().setX(
+                            transform.getPosition().getX()
+                                    + ((rigidBody.getVelocity().getX() * -1) * deltaTime));
+                }
+                return;
+            }
+
+            if (onKeyPressedEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
+                for (var entity : getSystemEntities()) {
+                    var transform = (TransformComponent) entity.getComponent(TransformComponent.COMPONENT_TYPE_ID);
+                    var rigidBody = (RigidBodyComponent) entity.getComponent(RigidBodyComponent.COMPONENT_TYPE_ID);
+
+                    transform.getPosition().setX(
+                            transform.getPosition().getX()
+                                    + ((rigidBody.getVelocity().getX() * 1) * deltaTime));
+
+                    LogService.getInstance().error("deltaTime: " + deltaTime);
+                }
+            }
+
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public int getTypeId() {
+        return SYSTEM_TYPE_ID;
+    }
 }
