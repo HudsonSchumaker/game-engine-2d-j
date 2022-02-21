227e2451da3f38d6c6fd78a5ec9d5577a599fd5b
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-21 12:12:56 +0100

    Refactoring.

--- src/main/java/com/schumakerteam/alpha/systems/Updatable.java
+++ src/main/java/com/schumakerteam/alpha/systems/Updatable.java
@@ -3,7 +3,7 @@ package com.schumakerteam.alpha.systems;
 import java.awt.*;
 
 public interface Updatable {
-
     default void update() {}
+    default void update(double deltaTime) {}
     default void update(Graphics2D g) {}
 }
