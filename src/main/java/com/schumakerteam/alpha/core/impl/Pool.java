4ffe36e5f64df4a25b373d1ae01e6b1eb090299b
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-21 12:12:30 +0100

    Refactoring.

--- src/main/java/com/schumakerteam/alpha/core/impl/Pool.java
+++ src/main/java/com/schumakerteam/alpha/core/impl/Pool.java
@@ -1,7 +1,7 @@
 package com.schumakerteam.alpha.core.impl;
 
 import com.schumakerteam.alpha.core.IPool;
-import com.schumakerteam.alpha.component.Component;
+import com.schumakerteam.alpha.ecs.impl.Component;
 
 import java.util.HashMap;
 import java.util.Map;
