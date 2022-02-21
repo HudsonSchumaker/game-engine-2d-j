95fac425a10854c1690a1b02a84effd352f24704
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-21 12:12:05 +0100

    Refactoring.

---
+++ src/main/java/com/schumakerteam/alpha/component/MovementComponent.java
@@ -0,0 +1,44 @@
+package com.schumakerteam.alpha.component;
+
+import com.schumakerteam.alpha.common.ComponentIdMap;
+import com.schumakerteam.alpha.ecs.impl.Component;
+import com.schumakerteam.alpha.ecs.impl.Registry;
+
+public class MovementComponent extends Component {
+
+    public static final int COMPONENT_TYPE_ID = ComponentIdMap.getTypeId(MovementComponent.class.getName());
+    private final int id;
+    private boolean move;
+
+    public MovementComponent() {
+        this(true);
+    }
+
+    public MovementComponent(boolean move) {
+        this.id = Registry.getInstance().getComponentId();
+        this.move = true;
+    }
+
+    public boolean isMoving() {
+        return move;
+    }
+
+    public void setMove(boolean move) {
+        this.move = move;
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public int getTypeId() {
+        return COMPONENT_TYPE_ID;
+    }
+
+    @Override
+    public String toString() {
+        return "MovementComponent{" + "id=" + id + ", move=" + move + '}';
+    }
+}
