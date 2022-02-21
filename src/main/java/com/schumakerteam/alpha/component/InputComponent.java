bf90692562ef8b56b061f6f0b4759f37654fb26b
HudsonSchumaker <hudson.schumaker@me.com>
2022-02-21 12:11:32 +0100

    Refactoring.

--- src/main/java/com/schumakerteam/alpha/component/InputComponent.java
+++ src/main/java/com/schumakerteam/alpha/component/InputComponent.java
@@ -1,4 +1,26 @@
 package com.schumakerteam.alpha.component;
 
+import com.schumakerteam.alpha.common.ComponentIdMap;
+import com.schumakerteam.alpha.ecs.impl.Component;
+import com.schumakerteam.alpha.ecs.impl.Registry;
+
 public class InputComponent extends Component {
+
+    public static final int COMPONENT_TYPE_ID = ComponentIdMap.getTypeId(InputComponent.class.getName());
+    private final int id;
+
+    public InputComponent() {
+        this.id = Registry.getInstance().getComponentId();
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public int getTypeId() {
+        return COMPONENT_TYPE_ID;
+    }
+    // TODO toString
 }
