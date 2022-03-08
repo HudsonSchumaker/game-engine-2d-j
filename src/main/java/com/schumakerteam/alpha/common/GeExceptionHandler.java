77b41dfa7c464a1543e1a5912ab4f5dbc1c32e00
Hudson Schumaker <hudson.schumaker@me.com>
2022-03-08 16:38:50 +0100

    Author.

--- src/main/java/com/schumakerteam/alpha/common/GeExceptionHandler.java
+++ src/main/java/com/schumakerteam/alpha/common/GeExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.schumakerteam.alpha.common;
 
+/**
+ * @author Hudson Schumaker
+ */
 public final class GeExceptionHandler implements Thread.UncaughtExceptionHandler {
     @Override
     public void uncaughtException(Thread t, Throwable e) {
