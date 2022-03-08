f1124a4236d72bd9bd3820e67823fd8dbf119a32
Hudson Schumaker <hudson.schumaker@me.com>
2022-03-08 16:40:14 +0100

    Author.

--- src/main/java/com/schumakerteam/alpha/component/AnimationComponent.java
+++ src/main/java/com/schumakerteam/alpha/component/AnimationComponent.java
@@ -5,6 +5,9 @@ import com.schumakerteam.alpha.ecs.impl.Component;
 import com.schumakerteam.alpha.ecs.impl.Registry;
 import com.schumakerteam.alpha.log.LogService;
 
+/**
+ * @author Hudson Schumaker
+ */
 public final class AnimationComponent extends Component {
 
     public static final int COMPONENT_TYPE_ID = ComponentIdMap.getTypeId(AnimationComponent.class.getName());
