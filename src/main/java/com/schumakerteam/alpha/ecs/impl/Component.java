e3dd6f8db249222420704f15e33dede7a567d33b
Hudson Schumaker <hudson.schumaker@me.com>
2022-03-09 22:55:18 +0100

    Author.

--- src/main/java/com/schumakerteam/alpha/ecs/impl/Component.java
+++ src/main/java/com/schumakerteam/alpha/ecs/impl/Component.java
@@ -1,5 +1,8 @@
 package com.schumakerteam.alpha.ecs.impl;
 
+/**
+ * @author Hudson Schumaker
+ */
 public abstract class Component {
     public abstract int getId();
     public abstract int getTypeId();
