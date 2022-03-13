6e05ff6740ff01b4d0b4eefceaed55588f40e41f
HudsonSchumaker <hudson.schumaker@me.com>
2022-03-13 09:32:15 +0100

    @Author.

--- src/main/java/com/schumakerteam/alpha/Main.java
+++ src/main/java/com/schumakerteam/alpha/Main.java
@@ -2,6 +2,9 @@ package com.schumakerteam.alpha;
 
 import com.schumakerteam.alpha.core.impl.Game;
 
+/**
+ * @author Hudson Schumaker
+ */
 public class Main {
 
     public static void main(String ...args) {
